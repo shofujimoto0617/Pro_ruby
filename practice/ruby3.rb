d6152b3b469fce056c0d1564d885b5011a106cec
shofujimoto0617 <shofujimoto017@gmail.com>
2021-03-10 23:07:42 +0900

    practice ruby

--- practice/ruby3.rb
+++ practice/ruby3.rb
@@ -307,25 +307,25 @@
 
 # <<  -------------------------- >>
 
-puts "A~Zのいずれかの1文字を入力してください"
+# puts "A~Zのいずれかの1文字を入力してください"
 
-zero = ["C", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-one = ["A", "D", "O", "P", "Q", "R"]
-two = "B"
+# zero = ["C", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+# one = ["A", "D", "O", "P", "Q", "R"]
+# two = "B"
 
-s = gets.chomp
+# s = gets.chomp
 
-if s.length == 1
-	if zero.include?(s)
-		puts "0"
-	elsif one.include?(s)
-		puts "1"
-	else 
-		puts "2"
-	end
-else
-	puts "エラー(A ~ Z のいずれかの1文字)"
-end
+# if s.length == 1
+# 	if zero.include?(s)
+# 		puts "0"
+# 	elsif one.include?(s)
+# 		puts "1"
+# 	else 
+# 		puts "2"
+# 	end
+# else
+# 	puts "エラー(A ~ Z のいずれかの1文字)"
+# end
 
 # <<  -------------------------- >>
 
@@ -367,5 +367,13 @@ end
 		
 
 		
+# <<  -------------------------- >>
 
+while m = gets.to_i
+	if m < 0 || m > 100000
+		puts "エラー(0 ≤ m ≤ 100000)"
+	else
+		break puts m / 10
+	end
+end
 
